2bfb87bb36ed49285eb102757e3794ae4fbb9830
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2018-03-09 10:54:57 +0100

    [misc] Add the first version of the websites
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

---
+++ build/production.sh
@@ -0,0 +1,17 @@
+pwd
+cd ../
+pwd
+
+if [ -d acceleo/common_assets ]; then rm -R acceleo/common_assets; fi
+mkdir ./acceleo/common_assets
+cp -r ./common_assets/* ./acceleo/common_assets
+
+if [ -d sirius/common_assets ]; then rm -R sirius/common_assets; fi
+mkdir ./sirius/common_assets
+cp -r ./common_assets/* ./sirius/common_assets
+
+if [ -d umldesigner/common_assets ]; then rm -R umldesigner/common_assets; fi
+mkdir ./umldesigner/common_assets
+cp -r ./common_assets/* ./umldesigner/common_assets
+
+bundle exec jekyll build --config=_production_config.yml
